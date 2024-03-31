796c6d1ad4e012b043b50a5a572cc6d20819fe90
Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2024-03-31 17:51:35 +0600

    responsive complete

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -25,15 +25,11 @@ const Navber = () => {
                     <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Upcomming</a>
                 </li>
             </ul>
-            <div className="items-center flex-shrink-0 hidden lg:flex">
-                <Link to='/login'><button className="self-center px-8 py-3 rounded">Sign in</button></Link>
+            <div className="items-center flex-shrink-0  lg:flex">
+                <Link to='/login'><button className="self-center px-8 py-3 rounded bg-green-500 mr-2">Sign in</button></Link>
                 <Link to='/regester'><button className="self-center px-8 py-3 font-semibold rounded bg-violet-400 text-gray-900">Sign up</button></Link>
             </div>
-            <button className="p-4 lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-100">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-            </button>
+           
         </div>
     </header>
     );
